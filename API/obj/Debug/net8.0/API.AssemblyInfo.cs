167650ac427fa364e7d9bfad4599c22b4313a04d
JuanFabris <144691483+JuanFabris@users.noreply.github.com>
2024-11-02 15:37:19 +0100

    BuilderSettedUp

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c419d8d09c8b10d713cb9af79d2af57768821b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d710246b31762a3c8482571e08bdcc7861d02cdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
